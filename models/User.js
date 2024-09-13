de3242df3fd02d65ea89363b8ae71834feaecb3f
Ivan lg <boogue66@gmail.com>
2024-09-13 11:04:53 -0600

    update: findbyid

--- models/User.js
+++ models/User.js
@@ -29,6 +29,15 @@ class User {
     await db.query(query);
   }
   // Encontrar un usuario por email
+  static async findById (id) {
+    try {
+      const [rows] = await db.query('SELECT * FROM users WHERE id = ?', [email]);
+      return rows[0];
+    } catch (error) {
+      console.error('Error al buscar usuario por email:', error);
+      throw new Error('Error al buscar usuario');
+    }
+  }
   static async findByEmail(email) {
     try {
       const [rows] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
